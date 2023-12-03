716d614aa980d159c6c815c22915b910fa9cb27c
SumukhiPrasad <sumukhi_prasad@berkeley.edu>
2023-12-02 22:20:28 -0800

    finished final project

--- Procrastination_Is_a_Way_of_Life.py
+++
@@ -1,5 +0,0 @@
-#the title shines light on how i procrastinated on this homework assignment
-print((2*60*60)/(728496/12/60708*60*60*24*7*(5000/5/1000))) #this is the amount of time each week I realistically spend on homework 
-
-#I would run the file by using the play button in Visual Studio Code. You might be able to hit enter to make it play as well. 
-
